e89c3c2108aa4c61af10b1f082b763aadfaa0321
Nyvalldo <serafim739@gmail.com>
2020-10-10 09:00:53 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/serafim/workshopmongo/resources/UserResource.java
+++ src/main/java/com/serafim/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.serafim.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -7,13 +8,17 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.serafim.workshopmongo.domain.User;
 import com.serafim.workshopmongo.dto.UserDTO;
 import com.serafim.workshopmongo.services.UserService;
+
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -33,5 +38,19 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		Optional<User> obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO());
+	}
+	
+	@RequestMapping( method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/serafim/workshopmongo/services/UserService.java
+++ src/main/java/com/serafim/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.serafim.workshopmongo.domain.User;
+import com.serafim.workshopmongo.dto.UserDTO;
 import com.serafim.workshopmongo.repository.UserRepository;
 import com.serafim.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -27,4 +28,18 @@ public class UserService {
 		}
 		return user;
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
+//só pra salvar
