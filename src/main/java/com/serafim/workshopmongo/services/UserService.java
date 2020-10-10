6b4411a685228c9816afcec22b6dcbd756286c94
Nyvalldo <serafim739@gmail.com>
2020-10-09 21:35:12 -0300

    Obtendo um usuário por id

--- src/main/java/com/serafim/workshopmongo/services/UserService.java
+++ src/main/java/com/serafim/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.serafim.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.serafim.workshopmongo.domain.User;
 import com.serafim.workshopmongo.repository.UserRepository;
+import com.serafim.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public Optional<User> findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
